66936dcffeba46c8270f6d734fb6a3c5c813f9e7
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-10-27 18:11:25 -0300

    update: nova arquitetura. v2

--- models/base.js
+++ models/base.js
@@ -1,5 +1,9 @@
 export default class Model{
     constructor(){
-        
+        this.ID = undefined
+        this.DtCriacao = undefined
+        this.DtAlteracao = undefined
+        this.DtRemocao = undefined
+        this.FlRemocao = undefined
     }
 }
